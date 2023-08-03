4f94a9646240ea26c926e6c2a3c7f27af3f555af
HP <HP@DESKTOP-4J7IFI2>
2023-08-03 20:04:11 +0530

    Samplecommit is committed

---
+++ src/test/java/Sample/Samplecommit.java
@@ -0,0 +1,13 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Samplecommit {
+
+	@Test
+	
+	public void commit()
+	{
+		System.out.println("Github commit and push done successfully");
+	}
+}
