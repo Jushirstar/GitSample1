e4ce861f6f8dc0f608b6cc6ab5de2327fc58cce4
HP <HP@DESKTOP-4J7IFI2>
2023-08-04 19:44:37 +0530

    gitt committed
    simplepractice committed

---
+++ src/test/java/Git/Gitt.java
@@ -0,0 +1,20 @@
+package Git;
+
+public class Gitt {
+
+	public static void main(String[] args) {
+		
+		int a= 15;
+		int b = 25;
+		if(a%b==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+
+	}
+
+}
---
+++ src/test/java/SimplePractice/SimplePractice.java
@@ -0,0 +1,15 @@
+package SimplePractice;
+
+import java.util.Scanner;
+
+public class SimplePractice {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		System.out.println(n + "is as input");
+	}
+
+}
